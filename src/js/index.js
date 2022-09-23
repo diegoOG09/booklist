0cdd62fabb8d0a1b3150a5603d90915d2c9f8d1c
diegoOG09 <xdiegoemmax@outlook.com>
2022-09-23 15:07:00 -0500

    API and client working

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const booksGridDiv = document.querySelector('.books-grid')
 
-  fetch('http://localhost:4567/api/v1/books').then(function (response) {
-    //The API call was succesfully!
-    console.log('succes!', response)
-  }).catch(function (err) {
-    // There was an error
-    console.log('Something went wrong', err)
+  fetch('http://localhost:4567/api/v1/books', {
+    method: 'get',
+    headers: {'Acces-Control-Allow-Origin': '*'}
   })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
 
 })
