cba2599f776832f76581c424609685b1ade931fe
diegoOG09 <xdiegoemmax@outlook.com>
2022-09-29 19:11:09 -0500

    books name displayed in html

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
     method: 'get',
     headers: {'Acces-Control-Allow-Origin': '*'}
   })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((response) => { return response.json() })
+    .then((data) => {
+      console.log(data)
+      let books = data
+
+      books.map(function(book) {
+        let div = document.createElement('div')
+        let title = document.createElement('h2')
+        let author = document.createElement('h3')
+        let isbn = document.createElement('span')
+
+        
+        title.innerHTML = `${book.title}`
+        author.innerHTML = `${book.author}`
+        isbn.innerHTML = `${book.isbn}`
+
+        div.appendChild(title, author, isbn)
+        booksGridDiv.appendChild(div)
+      })
+    })
 
 })
